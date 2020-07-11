fb8ac8e36b69c7553e57bc8d12e483cfb0109636
saul <sauld@cimat.mx>
2020-07-11 09:50:37 -0700

    Commit with common title fixed

--- r_code/confidence_bands.R
+++ r_code/confidence_bands.R
@@ -25,9 +25,9 @@ confidence_intervals_plots <-
   data_file_name <- 'data_mortality_rate.csv'
   raw_italy_mr_df <- fread(data_file_name, header = T, sep = ',')
   italy_mr_df <- filter(raw_italy_mr_df, Year <= 2004)
-  italy_mr_df_forcast <- filter(raw_italy_mr_df, Year > 2004, Year <=2012)
+  italy_mr_df_forcast <- filter(raw_italy_mr_df, Year > 2004, Year <= 2012)
   #
-  melted_data_mr <- melt(ht_hat, id=c("Age", "Sex"), measured = names[1:63], 
+  melted_data_mr <- melt(ht_hat, id = c("Age", "Sex"), measured = names[1:63], 
                          stringsAsFactors = FALSE )
   #
   names(melted_data_mr)[3] <- "Year"
@@ -40,25 +40,25 @@ confidence_intervals_plots <-
   #
   mr_confidence_band_by_age <- melted_data_mr %>%
     group_by(Age, Sex,Year) %>%
-      summarise(mean=mean(mr), LoCI = mean(mr) - 1.96 * sd(mr), 
+      summarise(mean = mean(mr), LoCI = mean(mr) - 1.96 * sd(mr), 
                 HiCI = mean(mr) + 1.96 * sd(mr))
   #
-  index <- seq(from = 1, to = nrow(mr_mean_by_age),  by=3)
+  index <- seq(from = 1, to = nrow(mr_mean_by_age),  by = 3)
   #
   p <- ggplot(mr_mean_by_age[index,], aes(x = Year,
                              y = mean,
                              color = Age,
                              shape = Sex,
                              group = interaction(Age, Sex))) +
-    geom_point(size=3) +
+    geom_point(size = 3) +
     geom_line() + # scale_color_grey()+
     theme_classic() +
-    theme(axis.text.x = element_text(size=12, angle=90)) +
+    theme(axis.text.x = element_text(size = 12, angle = 90)) +
     scale_color_gradientn(colours = rainbow(5))
   #
-  ggsave("mean_mortality_rate_by_ages.eps", width=golden_width, 
-         height=golden_height,
-         device="ps", units="mm")
+  ggsave("mean_mortality_rate_by_ages.eps", width = golden_width, 
+         height = golden_height,
+         device = "ps", units = "mm")
   #### Woman confidence band plot ####
   theme_set(
     theme_bw() +
@@ -68,82 +68,70 @@ confidence_intervals_plots <-
   colors <- c("Mean" = "red", 
               "Confidence" = "grey80", 
               "Italy Data" = "black",
-              "Forecasting" ="blue")
+              "Forecasting" = "blue")
   plotlist <- list()
   cont <- 1
-  for (k in ages){
+  for (k in ages) {
     woman_mr_by_age_k_summary <- filter(mr_confidence_band_by_age, 
-                                        Age==k, Sex=='F')
+                                        Age == k, Sex == 'F')
     woman_mr_by_age_k_summary$Year <- 
       as.numeric(as.character(woman_mr_by_age_k_summary$Year))
-    mr_italy_by_age_k <- filter(italy_mr_df, Age==k)
-    mr_italy_forecast_by_age_k <- filter(italy_mr_df_forcast, Age==k)
+    mr_italy_by_age_k <- filter(italy_mr_df, Age == k)
+    mr_italy_forecast_by_age_k <- filter(italy_mr_df_forcast, Age == k)
     woman_mr_italy_by_age_k <- select(mr_italy_by_age_k, Year, Female )
     woman_mr_italy_forcast_by_age_k <- 
       select(mr_italy_forecast_by_age_k, Year, Female )
     str_title <- paste("Age:", toString(k))
     #
-    if (cont==1){
+    if (cont == 1) {
       p_age_k <- ggplot(woman_mr_by_age_k_summary, aes(Year, mean)) +
         geom_line(aes(y = LoCI)) +
         geom_line(aes(y = HiCI)) +
-        geom_ribbon( aes(ymin=LoCI, ymax=HiCI, fill="Confidence")) +
-        geom_line(aes(y = mean, colour = "Mean"), size=1.0) +
+        geom_ribbon( aes(ymin = LoCI, ymax = HiCI, fill = "Confidence")) +
+        geom_line(aes(y = mean, colour = "Mean"), size = 1.0) +
         geom_point(data = woman_mr_italy_by_age_k,
-                   aes(x = Year, y=Female, colour="Italy Data")) +
-        #geom_point(data = woman_mr_italy_forcast_by_age_k,
-        #           aes(x = Year, y=Female, colour="Italy Data 2004-20014"),
-        #           shape = 21, fill = "lightgray",
-        #           color = "black", size = 1) +
+                   aes(x = Year,
+                       y = Female,
+                       colour = "Italy Data")) +
         geom_point(data = woman_mr_italy_forcast_by_age_k,
-                   aes(x = Year, y=Female, colour="Forecasting")) +
+                   aes(x = Year, y = Female, colour = "Forecasting")) +
         ggtitle(str_title) +
         scale_color_manual(values = colors,  
                            name = "", 
-                           guide = guide_legend(override.aes=aes(fill=NA))) +
-        scale_fill_manual(values = "grey80", name="") +
-        labs(x = "Year", y = "Mortality Rate",  color = "Legend") +
+                           guide = guide_legend(override.aes = 
+                                                  aes(fill = NA))) +
+        scale_fill_manual(values = "grey80", name = "") +
+        labs(x = "Year", y = "Mortality Rate", color = "Legend") +
         theme_classic() +
         theme(plot.title = element_text(hjust = 0.5)) +
-        theme(axis.text.x = element_text(size=10, angle=90))
+        theme(axis.text.x = element_text(size = 10, angle = 90))
     }
     else{
       p_age_k <- ggplot(woman_mr_by_age_k_summary, aes(Year, mean)) +
         geom_line(aes(y = LoCI)) +
         geom_line(aes(y = HiCI)) +
-        geom_ribbon( aes(ymin=LoCI, ymax=HiCI, fill="Confidence")) +
-        geom_line(aes(y = mean, color="Mean"), size=1) +
+        geom_ribbon( aes(ymin = LoCI, ymax = HiCI, fill = "Confidence")) +
+        geom_line(aes(y = mean, color = "Mean"), size = 1) +
         geom_point(data = woman_mr_italy_by_age_k, 
-                   aes(x = Year, y=Female, color="Italy Data")) +
+                   aes(x = Year, y = Female, color = "Italy Data")) +
         geom_point(data = woman_mr_italy_forcast_by_age_k,
-                   aes(x = Year, y=Female, colour="Forecasting")) +
+                   aes(x = Year, y = Female, colour = "Forecasting")) +
         #
         ggtitle(str_title) +
-        scale_fill_manual(values = "grey80", name="Band") +
+        scale_fill_manual(values = "grey80", name = "Band") +
         labs(x = "Year", y = "Mortality Rate",  color = "Legend") +
         scale_color_manual(values = colors, 
                            name = "",
-                           guide = guide_legend(override.aes=aes(fill=NA))) +
+                           guide = guide_legend(override.aes = 
+                                                  aes(fill = NA))) +
         theme_classic() +
         theme(plot.title = element_text(hjust = 0.5)) +
-        theme(axis.text.x = element_text(size=10, angle=90))
+        theme(axis.text.x = element_text(size = 10, angle = 90))
     }
      plotlist[[length(plotlist) + 1]] <- p_age_k
      cont <- cont + 1
   }
-  # figure <- ggarrange(plotlist[[1]],
-  #                     plotlist[[2]],
-  #                     plotlist[[3]],
-  #                     plotlist[[4]],
-  #                     labels = c("A", "B", "C"),
-  #                     ncol = 2, nrow = 2)
-  # file_name <-"woman_confidence_bands.eps"
-  # ggsave(file_name, 
-  #        width=golden_width,
-  #        height=golden_height,
-  #        device="ps",
-  #        units="mm",
-  #        figure)
+  #
   grid_arrange_shared_legend(plotlist[[1]],
                              plotlist[[2]],
                              plotlist[[3]],
@@ -151,55 +139,68 @@ confidence_intervals_plots <-
                              nrow = 2,
                              ncol = 2,
                              position = c("bottom", "right"),
-                             file_name='woman_confidence_bands.eps')
+                             common_title_plot = "Woman",
+                             file_name = 'woman_confidence_bands.eps')
 
   #### Man confidence band plot ####
-  for (k in ages){
+  k <- 0
+  plotlist <- list()
+  cont <- 1
+  for (k in ages) {
     man_mr_by_age_k_summary <- filter(mr_confidence_band_by_age, 
-                                      Age==k, Sex=='M')
+                                      Age == k, Sex == 'M')
     man_mr_by_age_k_summary$Year <- 
       as.numeric(as.character(man_mr_by_age_k_summary$Year))
-    mr_italy_by_age_k <- filter(italy_mr_df, Age==k)
+    mr_italy_by_age_k <- filter(italy_mr_df, Age == k)
     man_mr_italy_by_age_k <- select(mr_italy_by_age_k, Year, Male )
+    man_mr_italy_forcast_by_age_k <- 
+      select(mr_italy_forecast_by_age_k, Year, Male)
     str_title <- paste("Age:", toString(k))
     #
-    if (cont==1){
+    if (cont == 1) {
       p_age_k <- ggplot(man_mr_by_age_k_summary, aes(Year, mean)) +
         geom_line(aes(y = LoCI)) +
         geom_line(aes(y = HiCI)) +
-        geom_ribbon( aes(ymin=LoCI, ymax=HiCI, fill="Confidence at 95%")) +
-        geom_line(aes(y = mean, colour = "Simulation-mean"), size=1.5) +
+        geom_ribbon( aes(ymin = LoCI, 
+                         ymax = HiCI, fill = "Confidence")) +
+        geom_line(aes(y = mean, colour = "Mean"), size = 1.5) +
         geom_point(data = man_mr_italy_by_age_k,
-                   aes(x = Year, y=Female, colour="Italy Data")) +
+                   aes(x = Year, y = Male, colour = "Italy Data")) +
+        geom_point(data = man_mr_italy_forcast_by_age_k,
+                   aes(x = Year, y = Male, colour = "Forecasting")) +
         ggtitle(str_title) +
         scale_color_manual(values = colors,
                            name = "",
-                           guide = guide_legend(override.aes=aes(fill=NA))) +
-        scale_fill_manual(values = "grey80", name="") +
+                           guide = guide_legend(override.aes = 
+                                                  aes(fill = NA))) +
+        scale_fill_manual(values = "grey80", name = "") +
         labs(x = "Year", y = "Mortality Rate",  color = "Legend") +
-         theme_classic() +
+        theme_classic() +
         theme(plot.title = element_text(hjust = 0.5)) +
-        theme(axis.text.x = element_text(size=10, angle=90))
+        theme(axis.text.x = element_text(size = 10, angle = 90))
     }
     else{
       p_age_k <- ggplot(man_mr_by_age_k_summary, aes(Year, mean)) +
         geom_line(aes(y = LoCI)) +
         geom_line(aes(y = HiCI)) +
-        geom_ribbon( aes(ymin=LoCI, 
-                         ymax=HiCI, fill="Confidence at 95%")) +
-        geom_line(aes(y = mean, color="Simulation-mean"), size=1.5) +
+        geom_ribbon( aes(ymin = LoCI, 
+                         ymax = HiCI, fill = "Confidence")) +
+        geom_line(aes(y = mean, color = "Mean"), size = 1.5) +
         geom_point(data = man_mr_italy_by_age_k,
-                   aes(x = Year, y=Female, color="Italy Data")) +
+                   aes(x = Year, y = Male, color = "Italy Data")) +
+        geom_point(data = man_mr_italy_forcast_by_age_k,
+                   aes(x = Year, y = Male, colour = "Forecasting")) +
         #
         ggtitle(str_title) +
-        scale_fill_manual(values = "grey80", name="Band") +
+        scale_fill_manual(values = "grey80", name = "Band") +
         labs(x = "Year", y = "Mortality Rate",  color = "Legend") +
         scale_color_manual(values = colors,
                            name = "",
-                           guide = guide_legend(override.aes=aes(fill=NA))) +
+                           guide = guide_legend(override.aes = 
+                                                  aes(fill = NA))) +
         theme_classic() +
         theme(plot.title = element_text(hjust = 0.5)) +
-        theme(axis.text.x = element_text(size=10, angle=90))
+        theme(axis.text.x = element_text(size = 10, angle = 90))
     }
      plotlist[[length(plotlist) + 1]] <- p_age_k
      cont <- cont + 1
@@ -211,5 +212,6 @@ confidence_intervals_plots <-
                              nrow = 2,
                              ncol = 2,
                              position = c("bottom", "right"),
-                             file_name='man_confidence_bands.eps')
+                             common_title_plot = "Man",
+                             file_name = 'man_confidence_bands.eps')
 }
--- r_code/shared_legend.R
+++ r_code/shared_legend.R
@@ -7,6 +7,7 @@ grid_arrange_shared_legend <- function(...,
                                        nrow = 1,
                                        ncol = length(list(...)),
                                        position = c("bottom", "right"),
+                                       common_title_plot,
                                        file_name='woman_confidence_bands.eps') {
   golden_ratio <- 1.61803398875
   golden_width <- 116
@@ -24,26 +25,36 @@ grid_arrange_shared_legend <- function(...,
                      "bottom" = arrangeGrob(do.call(arrangeGrob, gl),
                                             legend,
                                             ncol = 1,
-                                            heights = unit.c(unit(1, "npc") - lheight, lheight)),
+                                            heights = 
+                                              unit.c(unit(1, "npc") - 
+                                                       lheight, lheight)),
                      "right" = arrangeGrob(do.call(arrangeGrob, gl),
                                            legend,
                                            ncol = 2,
-                                           widths = unit.c(unit(1, "npc") - lwidth, lwidth)))
+                                           widths = unit.c(unit(1, "npc") -
+                                                             lwidth, lwidth)))
   grid.newpage()
   g <- grid.draw(combined)
   figure <- ggarrange(plots[[1]],
                       plots[[2]],
                       plots[[3]],
                       plots[[4]],
-                      labels = c("A", "B", "C"),
+                      labels = c("A", "B", "C", "D"),
                       ncol = 2, nrow = 2,
                       common.legend = TRUE, legend = "bottom"
-                      )
+                      ) %>%
+    annotate_figure(top = text_grob(common_title_plot,
+                                    color = "Black",
+                                    face = "bold",
+                                    size = 14),
+                    
+                    
+    )
   ggsave(file_name, 
-         width=golden_width,
-         height=golden_height,
-         device="ps",
-         units="mm",
+         width = golden_width,
+         height = golden_height,
+         device = "ps",
+         units = "mm",
          figure)
   return(g)
 }
